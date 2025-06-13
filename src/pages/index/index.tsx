bdfbf9422391fffdda6ca75e5430a0b7f0985e15
lirunhua <undercurre@163.com>
2025-06-13 21:38:37 +0800

    feat: 引入组件库

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,10 +1,12 @@
-import { Component, PropsWithChildren } from 'react'
-import { connect } from 'react-redux'
-import { View, Button, Text } from '@tarojs/components'
+import { Component, PropsWithChildren } from "react";
+import { connect } from "react-redux";
+import { View, Button, Text } from "@tarojs/components";
 
-import { add, minus, asyncAdd } from '../../actions/counter'
+import { add, minus, asyncAdd } from "../../actions/counter";
+import type { RootState } from "../../store";
+import { AtTabBar } from "taro-ui";
 
-import './index.scss'
+import "./index.scss";
 
 // #region 书写注意
 //
@@ -18,62 +20,84 @@ import './index.scss'
 
 type PageStateProps = {
   counter: {
-    num: number
-  }
-}
+    num: number;
+  };
+};
 
 type PageDispatchProps = {
-  add: () => void
-  dec: () => void
-  asyncAdd: () => any
-}
+  add: () => void;
+  dec: () => void;
+  asyncAdd: () => any;
+};
 
-type PageOwnProps = {}
+type PageOwnProps = {};
 
-type PageState = {}
+type PageState = {};
 
-type IProps = PageStateProps & PageDispatchProps & PageOwnProps
+type IProps = PageStateProps & PageDispatchProps & PageOwnProps;
 
 interface Index {
   props: IProps;
 }
-
-@connect(({ counter }) => ({
-  counter
-}), (dispatch) => ({
-  add () {
-    dispatch(add())
-  },
-  dec () {
-    dispatch(minus())
-  },
-  asyncAdd () {
-    dispatch(asyncAdd())
+@connect<PageStateProps, PageDispatchProps, PageOwnProps>(
+  (state: RootState) => ({
+    counter: state.counter,
+  }),
+  (dispatch: any) => ({
+    add() {
+      dispatch(add());
+    },
+    dec() {
+      dispatch(minus());
+    },
+    asyncAdd() {
+      dispatch(asyncAdd());
+    },
+  })
+)
+class Index extends Component<IProps> {
+  constructor(props: IProps) {
+    super(props);
+    this.state = {
+      current: 0,
+    };
   }
-}))
-class Index extends Component<PropsWithChildren> {
-  componentWillReceiveProps (nextProps) {
-    console.log(this.props, nextProps)
+
+  handleClick(value) {
+    this.setState({
+      current: value,
+    });
   }
 
-  componentWillUnmount () { }
+  componentWillReceiveProps(nextProps) {
+    console.log(this.props, nextProps);
+  }
 
-  componentDidShow () { }
+  componentWillUnmount() {}
 
-  componentDidHide () { }
+  componentDidShow() {}
 
-  render () {
+  componentDidHide() {}
+
+  render() {
     return (
-      <View className='index'>
-        <Button className='add_btn' onClick={this.props.add}>+</Button>
-        <Button className='dec_btn' onClick={this.props.dec}>-</Button>
-        <Button className='dec_btn' onClick={this.props.asyncAdd}>async</Button>
-        <View><Text>{this.props.counter.num}</Text></View>
-        <View><Text>Hello, World</Text></View>
+      <View className="index">
+        <AtTabBar
+          fixed
+          backgroundColor="#CBD5E0"
+          color="#1A202C"
+          selectedColor="#D4AF37"
+          tabList={[
+            { title: "模特", iconType: "user" },
+            { title: "换装", iconType: "shopping-bag" },
+            { title: "收藏", iconType: "heart" },
+          ]}
+          onClick={this.handleClick.bind(this)}
+          current={this.state.current}
+        />
       </View>
-    )
+    );
   }
 }
 
-export default Index
-
+export default Index;
